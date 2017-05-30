f7f5db50e248b173eeaa52951311ac4019231f52
shibafu <shibafu528@gmail.com>
2017-05-31 00:42:46 +0900

    Dispose可能であることを表すインターフェースを追加

---
+++ src/main/java/info/shibafu528/yukari/exvoice/Disposable.java
@@ -0,0 +1,8 @@
+package info.shibafu528.yukari.exvoice;
+
+/**
+ * Created by shibafu on 2017/05/30.
+ */
+public interface Disposable {
+    void dispose();
+}
--- src/main/java/info/shibafu528/yukari/exvoice/ProcWrapper.java
+++ src/main/java/info/shibafu528/yukari/exvoice/ProcWrapper.java
@@ -5,7 +5,7 @@ package info.shibafu528.yukari.exvoice;
  *
  * Created by shibafu on 2016/05/05.
  */
-public class ProcWrapper {
+public class ProcWrapper implements Disposable {
 
     private MRuby mRuby;
     private long rProcPointer;
