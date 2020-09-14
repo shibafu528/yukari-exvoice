072eddef50e6f39e190e745887cbb45ab92e59e2
shibafu <shibafu528@gmail.com>
2020-09-15 00:23:14 +0900

    Miquire.loadAll() の戻り値変換のミスを修正

--- src/main/jni/jni_miquire_Miquire.c
+++ src/main/jni/jni_miquire_Miquire.c
@@ -1,6 +1,7 @@
 #include <jni.h>
 #include <mruby.h>
 #include <mruby/mix.h>
+#include <mruby/array.h>
 #include <mruby/string.h>
 #include "generator.h"
 #include "converter.h"
@@ -29,7 +30,24 @@ JNIEXPORT jobjectArray JNICALL Java_info_shibafu528_yukari_exvoice_miquire_Miqui
         return NULL;
     }
 
-    jobject jResult = convertMrbValueToJava(env, mrb, result);
+    jclass stringArrayClass = (*env)->FindClass(env, JSIG_ARRAY(JSIG_CLASS("java/lang/String")));
+    jclass stringClass = (*env)->FindClass(env, "java/lang/String");
+    jobjectArray jResult = (*env)->NewObjectArray(env, 2, stringArrayClass, NULL);
+
+    mrb_value rSuccess = mrb_ary_entry(result, 0);
+    jobjectArray jSuccess = (*env)->NewObjectArray(env, RARRAY_LEN(rSuccess), stringClass, NULL);
+    for (int i = 0; i < RARRAY_LEN(rSuccess); i++) {
+        (*env)->SetObjectArrayElement(env, jSuccess, i, convertMrbValueToJava(env, mrb, mrb_ary_entry(rSuccess, i)));
+    }
+    (*env)->SetObjectArrayElement(env, jResult, 0, jSuccess);
+
+    mrb_value rFailure = mrb_ary_entry(result, 1);
+    jobjectArray jFailure = (*env)->NewObjectArray(env, RARRAY_LEN(rFailure), stringClass, NULL);
+    for (int i = 0; i < RARRAY_LEN(rFailure); i++) {
+        (*env)->SetObjectArrayElement(env, jFailure, i, convertMrbValueToJava(env, mrb, mrb_ary_entry(rFailure, i)));
+    }
+    (*env)->SetObjectArrayElement(env, jResult, 1, jFailure);
+
     if (mrb->exc) {
         jclass exceptionClass = (*env)->FindClass(env, JCLASS_EXVOICE_MRUBY_EXCEPTION);
         mrb_value ins = mrb_inspect(mrb, mrb_obj_value(mrb->exc));
